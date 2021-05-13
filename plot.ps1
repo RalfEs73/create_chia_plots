60d383f43f5c144a5a6dafbf34225466c9e35dbb
RalfEs73 <ralf@essbaumer.de>
2021-05-13 20:19:58 +0200

    Update plot.ps1

--- plot.ps1
+++ plot.ps1
@@ -1,6 +1,3 @@
-$temp_path = "C:\Chia\Plot"
-$farming_path = "D:\Crypto\Chia\Farm"
-
 $Pool_Public_Key = "885614edddbedb105b1effad1e6449ff2ca6595906c4036e1fa76ad6210bc2f0e01121a084123187c37d80c3b08d289a"
 $Farmer_Public_Key = "a690fa842e4081099b7813eba46609892d88d9d2b263a6f010f398e10b1df52b7c31b71dc4a6c93bffa1bc4a91b154e6"
 $Plots_erstellen = 1
@@ -9,6 +6,8 @@ $Threads = 2
 $Memory = 4608
 
 $app_version = "1.1.5"
+$temp_path = "C:\Chia\Plot"
+$farming_path = "D:\Crypto\Chia\Farm"
 
 $CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $CurrentUserName = $CurrentUser.split("\")[1]
