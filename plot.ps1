0e5ecd329abb4a7ecc1de79e82bab7e58588eb82
RalfEs73 <ralf@essbaumer.de>
2021-05-13 20:12:53 +0200

    Update plot.ps1

--- plot.ps1
+++ plot.ps1
@@ -5,10 +5,14 @@ $Pool_Public_Key = "885614edddbedb105b1effad1e6449ff2ca6595906c4036e1fa76ad6210b
 $Farmer_Public_Key = "a690fa842e4081099b7813eba46609892d88d9d2b263a6f010f398e10b1df52b7c31b71dc4a6c93bffa1bc4a91b154e6"
 $Plots_erstellen = 1
 
+$Threads = 2
+$Memory = 4608
 $CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $CurrentUserName = $CurrentUser.split("\")[1]
 $app_version = "1.1.5"
 
-Write-Host "Erstelle Plot"
-chia.exe plots create -f $Farmer_Public_Key -p $Pool_Public_Key -k 32 -n $Plots_erstellen -r 2 -b 4608 -t $temp_path -d $farming_path
-Write-Host "Done"
+Set-Location -Path C:\Users\$CurrentUserName\AppData\Local\chia-blockchain\app-$app_version\resources\app.asar.unpacked\daemon
+
+chia.exe plots create -f $Farmer_Public_Key -p $Pool_Public_Key -k 32 -n $Plots_erstellen -r $Threads -b $Memory -t $temp_path -d $farming_path
+
+Set-Location -Path D:\GitHub\create_chia_plots
